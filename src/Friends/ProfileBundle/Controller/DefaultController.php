0073212434236f5561191bb86af669e43057cea4
Thilanka <lgtkaushalya@gmail.com>
2015-11-08 07:12:49 +0530

    Added Friends/ProfileBundle Bundle

---
+++ src/Friends/ProfileBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Friends\ProfileBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FriendsProfileBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Friends/ProfileBundle/FriendsProfileBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Friends\ProfileBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FriendsProfileBundle extends Bundle
+{
+}
